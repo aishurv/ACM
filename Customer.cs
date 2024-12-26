3df34674ffc6326f3e4c452a833f73c3ab1e79d1
Laxmi Wandare <Laxmi.wandare@klingelenberg.com>
2024-12-26 14:04:53 +0530

    adding Customer OrderItem orders  and Product Classes

---
+++ Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM
+{
+    public class Customer
+    {
+        public Customer()
+        {
+
+        }
+        public Customer(int customerId)
+        {
+            CustomerID = customerId;
+        }
+        
+        public int CustomerID { get; set; }
+        public static int InstanceCount { get; set; }
+        public string EmailAddress { get; set; }
+        public string LastName{ get; set; }
+        public string FirstName{ get; set; }
+
+
+        public string FullName
+        {
+            get 
+            {
+                String fullName = FirstName;
+                if (!string.IsNullOrWhiteSpace(LastName))
+                {
+                    if (!string.IsNullOrWhiteSpace(FirstName))
+                    {
+                        fullName += " " ;
+                    }
+                    fullName += LastName;
+                }
+                return fullName;    
+            }
+        }
+        public bool Validate()
+        {
+            var isValide =true;
+            if (string.IsNullOrWhiteSpace(FirstName)) isValide = false;
+            if (string.IsNullOrWhiteSpace(LastName)) isValide = false;
+
+            return isValide;
+        }
+        public Customer Retrive(int customerID) 
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrive()
+        {
+            return new List<Customer>();
+        }
+
+
+
+
+
+    }
+}
---
+++ CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ACM
+{
+    public class CustomerRepository
+    {
+        public Customer Retrieve(int customerId)
+        {
+            Customer customer = new Customer(customerId);
+
+            if (customerId == 1)
+            {
+                customer.EmailAddress = "aishuWandare.me";
+                customer.FirstName = "Aishu";
+                customer.LastName = "Wandare";
+            }
+            return customer;
+        }
+
+        public bool Save(Customer customer)
+        {
+
+            return true;
+        }
+
+    }
+}
